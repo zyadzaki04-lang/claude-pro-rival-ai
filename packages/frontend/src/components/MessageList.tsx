fa3ce7aaf9c659b513dfa094a88c59b990a3b153
zyadzaki04-lang <zyadzaki04@gmail.com>
2026-08-25 19:52:25 +0200

    feat: add MessageList component

---
+++ packages/frontend/src/components/MessageList.tsx
@@ -0,0 +1,16 @@
+import { Message } from '../types'
+import MessageItem from './MessageItem'
+
+interface MessageListProps {
+  messages: Message[]
+}
+
+export default function MessageList({ messages }: MessageListProps) {
+  return (
+    <div className="space-y-4">
+      {messages.map((message) => (
+        <MessageItem key={message.id} message={message} />
+      ))}
+    </div>
+  )
+}
